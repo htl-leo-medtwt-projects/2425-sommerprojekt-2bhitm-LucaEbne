e864d981b93e357bdd2a0081c30f975ec0b6fea6
LucaEbne <l.ebner@students.htl-leonding.ac.at>
2025-03-20 15:59:43 +0100

    Animation bei der Slideshow hinzugefügt und angefangen dark Mode zu machen

--- project/js/main_script.js
+++ project/js/main_script.js
@@ -3,11 +3,15 @@ let slides = {
     places: ['./img/FamousPlacesSlideshow/FamousPlace2.jpg', './img/FamousPlacesSlideshow/FamousPlace3.jpg', './img/FamousPlacesSlideshow/matsushima.webp'],
     events: ['./img/FestivalsSlideshow/Festival1.jpg', './img/FestivalsSlideshow/Festival2.jpg', './img/FestivalsSlideshow/Festival3.jpg']
 };
-
+let headerBox = document.getElementById('header1');
+let root = document.querySelector(':root');
+let darkMode = false;
+let time;
 let currentImages = [];
 let currentIndex = 0;
 
 function openSlideshow(category) {
+    startBlossoms();
     currentImages = slides[category];
     currentIndex = 0;
     document.getElementById('slideshow-image').src = currentImages[currentIndex];
@@ -15,6 +19,7 @@ function openSlideshow(category) {
 }
 
 function closeSlideshow() {
+    endBlossom();
     document.getElementById('overlay').style.display = 'none';
 }
 
@@ -36,4 +41,38 @@ function slideLeft() {
         img.src = currentImages[currentIndex];
         img.style.opacity = 1;
     }, 800);
+}
+
+function startBlossoms() {
+   time = setInterval(createBlossom, 100);
+}
+
+function createBlossom() {
+    const blossom = document.createElement("div");
+    blossom.classList.add("blossom");
+
+    const randomX = Math.random();
+    blossom.style.setProperty("--random-x", randomX);
+
+    document.body.appendChild(blossom);
+
+    setTimeout(() => {
+        blossom.remove();
+    }, 5000);
+}
+function endBlossom(){
+    clearInterval(time);
+}
+
+function ModeSwitch(){
+    
+    if(darkMode){
+        root.style.setProperty('--background-color', 'white');
+        darkMode = false;
+        headerBox.remove = `<div class="rectangle2"></div>`;
+    } else {
+        root.style.setProperty('--background-color', '#494949');
+        headerBox.innerHTML += `<div class="rectangle2"></div>`;
+        darkMode = true;
+    }
 }
