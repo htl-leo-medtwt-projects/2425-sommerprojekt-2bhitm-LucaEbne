abd5f894bb7712dd449b9b08f62043e2ba51e296
LucaEbne <l.ebner@students.htl-leonding.ac.at>
2025-05-18 21:47:59 +0200

    funktionaltität mit switchen der gerichte,cover,name,rating,opening-hours,usw.. per js erfolgreich unmgesetzt. ganz basic design für die boxen gemacht. Ausstehend: pfeil-buttons zu img buttons umsetzten, design feritg stellen und die restlichen restaurant covers suchen. 3h workflow

--- project/js/restaurant-seite.js
+++ project/js/restaurant-seite.js
@@ -1,14 +1,38 @@
 let test = document.getElementById("test");
-console.log(restaurants)
-for (let i = 0; i < restaurants[0].dishes.length; i++) {
-    test.innerHTML += `
-    <div>
-    <div class="rect"></div>
-    <h1 class="dishName">${restaurants[0].dishes[i].name}</h1>
-    <p class="dishPrice">Price:${restaurants[0].dishes[i].price_yen}¥</p>
-    
-    <img class="imgFood" src="../img/Restaurant1-Food/food${i+1}.jpg" alt="">
-  </div>
+let currentRestaurant = 0;
+
+renderRestaurant(currentRestaurant);
+
+document.getElementById("arrow-left2").addEventListener("click", () => {
+    currentRestaurant = (currentRestaurant - 1 + restaurants.length) % restaurants.length;
+    renderRestaurant(currentRestaurant);
+});
+
+document.getElementById("arrow-right2").addEventListener("click", () => {
+    currentRestaurant = (currentRestaurant + 1) % restaurants.length;
+    renderRestaurant(currentRestaurant);
+});
+
+function renderRestaurant(index) {
+    const restaurant = restaurants[index];
+    const dishContainer = document.getElementById("test");
+    dishContainer.innerHTML = "";
+
+    for (let i = 0; i < restaurant.dishes.length; i++) {
+        dishContainer.innerHTML += `
+        <div>
+            <div class="rect"></div>
+            <h1 class="dishName">${restaurant.dishes[i].name}</h1>
+            <p class="dishPrice">Price:<br>${restaurant.dishes[i].price_yen}¥</p>
+            <img class="imgFood" src="../img/Restaurant${index + 1}-Food/food${i + 1}.jpg" alt="">
+        </div>
+        `;
+    }
+
+    document.getElementById("cover2").src = `../img/Restaurant${index + 1}-Food/Restaurant${index + 1}.jpg`;
+    document.querySelector(".headerText2").innerHTML = `
+        <p>${restaurant.name}<br>${restaurant.location} – ★${restaurant.rating}</p>
     `;
+
+    document.getElementById("description").innerText = `Öffnungszeiten: ${restaurant.opening_hours}`;
 }
-  
